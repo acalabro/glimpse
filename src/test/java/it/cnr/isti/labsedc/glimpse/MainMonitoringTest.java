c1836ff5ef6459fa01de9fc57ffd1d2d14d05b3c
Antonello Calabrò <antonello.calabro@isti.cnr.it>
2017-01-13 15:59:52 +0100

    update

---
+++ src/test/java/it/cnr/isti/labsedc/glimpse/MainMonitoringTest.java
@@ -0,0 +1,24 @@
+package it.cnr.isti.labsedc.glimpse;
+
+public class MainMonitoringTest {
+
+//	@Before
+//	public void setUp() throws Exception {
+//	}
+//
+//	@Test
+//	public void testInitProps() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public void testInit() {
+//		fail("Not yet implemented"); // TODO
+//	}
+//
+//	@Test
+//	public void testMain() {
+//		fail("Not yet implemented"); // TODO
+//	}
+
+}
